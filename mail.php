a2b7d1623120f9d21268c7da59c79516f4332b00
anisonjazz <k.acestaff@gmail.com>
2026-05-05 10:27:49 +0900

    Add files via upload

--- mail.php
+++ mail.php
@@ -17,11 +17,11 @@ if (!$name || !$email || !$subject || !$message || !filter_var($email, FILTER_VA
     exit;
 }
 
-$to = 'singeryoh2020@gmail.com';
+$admin_to = 'singeryoh2020@gmail.com';
 
-$mail_subject = mb_encode_mimeheader('[お問い合わせ] ' . $subject, 'UTF-8', 'B');
-
-$mail_body = <<<EOT
+// 管理者への通知メール
+$admin_subject = mb_encode_mimeheader('[お問い合わせ] ' . $subject, 'UTF-8', 'B');
+$admin_body = <<<EOT
 星 陽子 オフィシャルサイト お問い合わせ
 ----------------------------------------
 お名前　　: {$name}
@@ -32,13 +32,40 @@ $mail_body = <<<EOT
 {$message}
 ----------------------------------------
 EOT;
+$admin_headers  = 'From: noreply@hoshiyoko.com' . "\r\n";
+$admin_headers .= 'Reply-To: ' . $email . "\r\n";
+$admin_headers .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n";
+$admin_headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
 
-$headers  = 'From: noreply@hoshiyoko.com' . "\r\n";
-$headers .= 'Reply-To: ' . $email . "\r\n";
-$headers .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n";
-$headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
+$result = mb_send_mail($admin_to, $admin_subject, $admin_body, $admin_headers);
 
-$result = mb_send_mail($to, $mail_subject, $mail_body, $headers);
+// お客様への自動返信メール
+$reply_subject = mb_encode_mimeheader('【星 陽子 オフィシャルサイト】お問い合わせを受け付けました', 'UTF-8', 'B');
+$reply_body = <<<EOT
+{$name} 様
+
+このたびはお問い合わせいただき、ありがとうございます。
+以下の内容でお問い合わせを受け付けました。
+
+担当者よりご連絡いたしますので、しばらくお待ちください。
+
+----------------------------------------
+件名　　　: {$subject}
+
+メッセージ:
+{$message}
+----------------------------------------
+
+※このメールは自動送信されています。
+※このメールに返信いただいてもお答えできません。
+
+星 陽子 オフィシャルサイト
+EOT;
+$reply_headers  = 'From: 星 陽子 オフィシャルサイト <singeryoh2020@gmail.com>' . "\r\n";
+$reply_headers .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n";
+$reply_headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
+
+mb_send_mail($email, $reply_subject, $reply_body, $reply_headers);
 
 if ($result) {
     header('Location: index.html?sent=1#contact');
